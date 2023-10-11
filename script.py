441b26753c5cbcaefc3989e230ee213c98953c3d
SH4DOW4RE <andreagoldorak1410@gmail.com>
2023-10-11 15:14:27 +0200

    Changed script.py

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ from modules.pasm import *
 
 print("Test print a number.")
 print(69)
-print("Puting epoch into rdi.")
+print("Puting epoch into rbp.")
 epoch()
 print("Printing epoch.")
 print(RBP)
